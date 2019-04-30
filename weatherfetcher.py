ea17f53bab63fe1e49e42aed53158fb7f5cf84ed
Allophanate <b.tenny@posteo.de>
2019-04-30 15:38:13 +0200

    added documentation

--- weatherfetcher.py
+++ weatherfetcher.py
@@ -1,3 +1,9 @@
+"""
+this module downloads the current weather for a given location from
+from openweatherbase.org and saves it as a json file.
+It makes use of the requests module for easy API interaction.
+"""
+
 #The requests module allows for easy connection to a web adress, without worrying about connection timeout etc.
 import requests
 #allows the encoding, decoding, writing and reading of json-files
@@ -6,23 +12,37 @@ import json
 from datetime import datetime as dt
 
 def load_config_file():
+    """
+    This function reads a config file in json formatting.
+    the parameter-value-pairs are returned as dictionary.
+    """
     with open("config.json") as config_file:
         config_data = json.load(config_file)
         return config_data
 
 config_data = load_config_file()
 
+
+# binding of the config parameters to global variables
 API_KEY = config_data.["api_key"]
 FILE_PATH = config_data.["file_path"]
 CITY_ID = config_data.["city_id"]
 
 
 def get_timestamp():
+    """
+    This function creates a string of the current time in the format yyyymmddhh.
+    It uses inbuilt formatting capabilities of the datetime object.
+    """
     current_time = dt.now()
     timestamp = current_time.strftime("%Y%m%d%H")
     return timestamp
 
 def create_filename():
+    """
+    This function concatenates several strings to create the full filename
+    including path for the file to be saved.
+    """
     path_to_file = FILE_PATH
     timestamp = get_timestamp()
     filename = path_to_file + timestamp + "raw_data.json"
