1d3d295d658b4340b5709b5c0eb94e2d5dbfdf53
Allophanate <b.tenny@posteo.de>
2019-04-30 09:47:17 +0200

    Created function to fetch and write weather data

--- weatherfetcher.py
+++ weatherfetcher.py
@@ -9,14 +9,13 @@ def load_config_file():
     with open("config.json") as config_file:
         config_data = json.load(config_file)
         return config_data
-        
-API_KEY = load_config_file().["api_key"]
-FILE_PATH = load_config_file().["file_path"]
 
+config_data = load_config_file()
 
-def get_api_key():
-    api_key = ""
-    return api_key
+API_KEY = config_data.["api_key"]
+FILE_PATH = config_data.["file_path"]
+CITY_ID = config_data.["city_id"]
+
 
 def get_timestamp():
     current_time = dt.now()
@@ -28,3 +27,24 @@ def create_filename():
     timestamp = get_timestamp()
     filename = path_to_file + timestamp + "raw_data.json"
     return filename
+    
+def fetch_weather():
+    """
+    This function makes use of the requests-module to get weather data
+    from openweathermap.org
+    """
+    
+    # constructs the correct api adress from the necessary parts
+    api_adress = "http://api.openweathermap.org/data/2.5/weather?id=" + CITY_ID + "&APPID=" + API_KEY
+    filename = create_filename()
+    
+    # actual fetchin of the weather data
+    data = requests.get(api_adress)
+    
+    # encode data as json
+    # the format is correct, but python needs to recognize it as json
+    json_data = data.json()
+    
+    # write date to file as json
+    with open(filename, 'w') as outfile:
+        json.dump(json_data, outfile)
